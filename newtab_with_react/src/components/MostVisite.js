e88628c63f7f013d1397aa81d02fa12a5c6ee6fc
iamnabob@126.com <iamnabob@126.com>
2018-03-15 16:55:04 +0800

    完成页面框架布局

--- newtab_with_react/src/components/MostVisite.js
+++ newtab_with_react/src/components/MostVisite.js
@@ -2,6 +2,7 @@
  * Created by zengtao on 2017/8/16.
  */
 import React from 'react';
+import Dialog from './dialog/DialogBox';
 
 export default class MostVisite extends React.Component {
     constructor(arg) {
@@ -11,11 +12,28 @@ export default class MostVisite extends React.Component {
     componentWillMount = ()=> {
     }
 
+    dialog = () => {
+        // Tooltip.show('the tooltip autoHide after 3s', 3000);
+        Dialog.confirm({
+            title: '添加站点',
+            content: '',
+            okText: '确认',
+            cancelText: '取消',
+            onOk: () => {
+                console.log('ok')
+            },
+            onCancel: () => {
+                console.log('cancel')
+            }
+        })
+    }
+
     render() {
 
         let list = [1,2,3,4,5,6,7,8].map((data,index)=>(
-            <a key={index} className="mv-empty-tile"></a>
-        ))
+            <a key={index} className="mv-empty-tile" onClick={this.dialog}></a>
+        ));
+
         return (
             <div id="most-visited" className="thumb-ntp">
                 <div id="mv-tiles" style={{width:680}}>
--- newtab_with_react/src/main.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-import { render } from 'react-dom';
-import './common/main.css';
-import App from './components/App';
-
-render((
-<App/>
-), document.getElementById('app'))
