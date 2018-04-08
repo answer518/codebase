1867de7d03e63a62b17cce2d8e2d28a468a3a437
iamnabob@126.com <iamnabob@126.com>
2018-04-08 17:19:51 +0800

    display default search engine

--- newtab_with_extension/src/js/search.js
+++ newtab_with_extension/src/js/search.js
@@ -22,6 +22,43 @@ inputbox.on('focus', function() {
     }
 })
 
+let searchForm = $('f')
+
+function setEngine(data) {
+	var arrTmp = data.url.split('?');
+    var action = arrTmp[0];
+    if (arrTmp[1]) {
+        var nameParam = arrTmp[1].match(/[\w\-]+=%[\w\-]+/g);
+        var keywordName = nameParam[0].split('=')[0];
+        var reg = new RegExp('&*' + nameParam + '&*', 'g');
+        var param = arrTmp[1].replace(reg, '&').replace(/^&+|&+$/g, '');
+
+        inputbox.setAttribute('name', keywordName);
+        // 添加搜索额外参数
+        param = param.replace(/^&+|&+$/g, ''); // 矫正首尾无效值
+        var arr = param.split('&');
+        var extraParam = document.createElement('div');
+        extraParam.className = 'extra-param'
+        var tmpl = '';
+        for (var i = 0; i < arr.length; i++) {
+            var n = arr[i].indexOf('=');
+            var key = arr[i].substring(0, n);
+            var value = arr[i].substring(n + 1);
+            tmpl += '<input type="hidden" name="' + key + '" value="' + value + '">';
+        }
+        searchForm.querySelector('.extra-param').remove();
+        searchForm.appendChild(extraParam);
+        extraParam.innerHTML = tmpl
+    }
+}
+
+// 默认搜索引擎
+setEngine({
+    'name': '百度',
+    'key': 'baidu.com',
+    'url': 'https://www.baidu.com/?ie=utf-8&wd=%s'
+});
+
 // inputbox.on('blur', function() {
 //     if (isFakeboxFocused()) { 
 //         setFakeboxFocus(true);
