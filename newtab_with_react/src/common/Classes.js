a8a5e2eb69144081ddd77d2bd273a2e48cae29c8
郭廷杰 <iamnabob@126.com>
2018-06-14 22:00:04 +0800

    remove newtab_react

--- newtab_with_react/src/common/Classes.js
+++
@@ -1,5 +0,0 @@
-export default {
-    DELAYED_HIDE_NOTIFICATION: 'mv-notice-delayed-hide',
-    HIDE_NOTIFICATION: 'mv-notice-hide',
-    GRID_MOVED: 'removed'
-}
--- newtab_with_react/src/components/dialog/util.js
+++
@@ -1,9 +0,0 @@
-export default {
-    bindMethods(methods, obj) {
-        methods.forEach(func => {
-            if(typeof obj[func] === 'function') {
-                obj[func] = obj[func].bind(this);
-            }
-        })
-    }
-}
