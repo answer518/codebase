6ee95e7f52f32f75993513949d78392a901e7626
果糖酱 <iamnabob@126.com>
2020-03-29 23:06:37 +0800

    修改json.js

--- jsonp/jsonp.js
+++ jsonp/jsonp.js
@@ -1,36 +1,43 @@
-const jsonp = function (url, data) {
-    return new Promise((resolve, reject) => {
-        // 初始化url
-        let dataString = url.indexOf('?') === -1 ? '?' : '&'
-        let callbackName = `jsonpCB_${Date.now()}`
-        url += `${dataString}callback=${callbackName}`
-        if (data) {
-            // 有请求参数，依次添加到url
-            for (let k in data) {
-                url += `&${k}=${data[k]}`
-            }
-        }
-        let jsNode = document.createElement('script')
-        jsNode.src = url
-        // 触发callback，触发后删除js标签和绑定在window上的callback
-        window[callbackName] = result => {
-            delete window[callbackName]
-            document.body.removeChild(jsNode)
-            if (result) {
-                resolve(result)
-            } else {
-                reject('没有返回数据')
-            }
-        }
-        // js加载异常的情况
-        jsNode.addEventListener('error', () => {
-            delete window[callbackName]
-            document.body.removeChild(jsNode)
-            reject('JavaScript资源加载失败')
-        }, false)
-        // 添加js节点到document上时，开始请求
-        document.body.appendChild(jsNode)
-    })
+let jsonpID = 0;
+
+function jsonp(url, timeout = 7500) {
+  const head = document.querySelector('head');
+  jsonpID += 1;
+
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script');
+    const callbackName = `jsonpCallback${jsonpID}`;
+
+    script.src = encodeURI(`${url}?callback=${callbackName}`);
+    script.async = true;
+
+    const timeoutId = window.setTimeout(() => {
+      cleanUp();
+
+      return reject(new Error('Timeout'));
+    }, timeout);
+
+    window[callbackName] = (data) => {
+      cleanUp();
+
+      return resolve(data);
+    };
+
+    script.addEventListener('error', (error) => {
+      cleanUp();
+
+      return reject(error);
+    });
+
+    function cleanUp() {
+      window[callbackName] = undefined;
+      head.removeChild(script);
+      window.clearTimeout(timeoutId);
+      script = null;
+    }
+
+    head.appendChild(script);
+  });
 }
 
 jsonp('http://192.168.0.103:8081/jsonp', { a: 1, b: 'heiheihei' })
